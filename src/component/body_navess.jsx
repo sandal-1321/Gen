4f546fa3dc3329251baa9197e54b01c3863ac179
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2025-09-12 14:03:00 +0530

    Add files via upload

---
+++ src/component/body_navess.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../static/body_navess.css'
+import gen from '../image/gen.png'
+
+const body_navess = () => {
+  return (
+    <div>
+        <div>
+          <div className='nav-title'>
+             <img src={gen} width="120px" height="45px"/>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default body_navess
