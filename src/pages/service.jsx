762ae77bee77eaa7fd36c2e2d16bd61b5bae2000
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2025-09-15 09:46:40 +0530

    Add files via upload

---
+++ src/pages/service.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../static/service.css"
+import data from "../file/data.json"
+
+const service = () => {
+    return (
+        <div className='main-service'>
+            <div className='service-0'>
+                <div className='service-titl'>
+                    <h1>Our Services</h1>
+                </div>
+                <div className='service-contain'>
+                    {data.map((val) => {
+                        return (
+                            <div className='ser-con-1'>
+                                <div className='ser-con-2'><i class={val.ic}></i></div>
+                                <h3>{val.titl}</h3>
+                                <p>{val.des}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default service
