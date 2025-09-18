91f246a797ac0a07c6eee57102a45f1bb212ff1d
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2025-09-18 19:20:48 +0530

    Add files via upload

--- src/component/body_navess.jsx
+++ src/component/body_navess.jsx
@@ -11,7 +11,7 @@ const BodyNavess = () => {
     setIsOpen((prev) => !prev);
   };
 
-  // Close when clicking outside
+  // Close menu when clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (menuRef.current && !menuRef.current.contains(event.target)) {
@@ -23,21 +23,26 @@ const BodyNavess = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
+  // Close menu when clicking a nav link
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="nav-main-2" ref={menuRef}>
       <div className="nav-sector">
         <div className="nav-title">
-          <Link to="/">
+          <Link to="/" onClick={handleLinkClick}>
             <img src={gen} width="120px" height="45px" alt="Logo" />
           </Link>
         </div>
         <div className="nav-lis">
           <ul className={isOpen ? "nav-lis-vis" : "nav-lis-hid"}>
-            <li><Link to="/"> Home </Link></li>
-            <li><Link to="/about"> About </Link></li>
-            <li><Link to="/services"> Our Services </Link></li>
-            <li><Link to="/blog"> Blog </Link></li>
-            <li><Link to="/contact"> Contact Us </Link></li>
+            <li><Link to="/" onClick={handleLinkClick}> Home </Link></li>
+            <li><Link to="/about" onClick={handleLinkClick}> About </Link></li>
+            <li><Link to="/services" onClick={handleLinkClick}> Our Services </Link></li>
+            <li><Link to="/blog" onClick={handleLinkClick}> Blog </Link></li>
+            <li><Link to="/contact" onClick={handleLinkClick}> Contact Us </Link></li>
           </ul>
 
           {/* Hamburger Icon */}
