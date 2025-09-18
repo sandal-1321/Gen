4e929d1e3621535050f27260b1d8260bf0c4008a
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2025-09-18 09:06:20 +0530

    Add files via upload

---
+++ src/file/servic_data.js
@@ -0,0 +1,45 @@
+export let Data = [
+  {
+    "id": 1,
+    "ic":"fa-solid fa-code",
+    "img":" /src/image/cloud.png",
+    "titl": "Cloud Solutions Development",
+    "des": "We provide comprehensive cloud solutions, from migration to management, ensuring scalability and security for your business."
+  },
+  {
+    "id": 2,
+    "ic":"fa fa-mobile",
+    "img":" /src/image/mobile.png",
+    "titl": "Mobile Application Development",
+    "des":"We develop secure, high-performance mobile applications for Android and iOS. With intuitive, user-friendly designs and engaging UI/UX, our apps deliver seamless experiences across all devices."
+    
+  },
+  {
+    "id": 3,
+    "ic":"fa-solid fa-code",
+    "img":" /src/image/banking.png",
+    "titl": "Banking Software Development",
+    "des": "We develop secure and scalable banking software solutions, including mobile banking apps and financial management systems."
+  },
+  {
+    "id": 4,
+    "ic":"fa fa-line-chart",
+    "img":" /src/image/e-commerce.avif",
+    "titl": "E-commerce Development",
+    "des":"We build e-commerce solutions that drive growth. Our online stores feature secure payments, streamlined product management, and a seamless shopping experience designed for high conversions."
+  },
+  {
+    "id": 5,
+    "ic":"fa fa-cogs",
+    "img":" /src/image/custom-software.jpg",
+    "titl": "Custom Software Development",
+    "des":"We craft tailored software solutions to solve your unique business challenges. Boost efficiency and drive innovation with applications designed to streamline your workflows."
+  },
+  {
+    "id": 6,
+    "ic":"fa fa-link",
+    "img":" /src/image/web.jpg",
+    "titl": "Web Development",
+    "des":"We craft scalable, secure, and responsive websites using the latest technologies. Our web development services empower your business with a strong, user-centric digital presence that drives results."
+  }
+]
