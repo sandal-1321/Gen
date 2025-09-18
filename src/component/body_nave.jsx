f21163d3ec341635f5fd01cdd9ab9a9056ec5378
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2025-09-18 16:11:45 +0530

    Add files via upload

---
+++ src/component/body_nave.jsx
@@ -0,0 +1,22 @@
+
+import '../static/body_nave.css'
+
+const body_nave = () => {
+  return (
+    <div>
+      <div className='nav-main-1'>
+        <div className='nav-conta'>
+          <div><span><i class="fa-solid fa-envelope"></i></span><p>GenBugMail@gmail.com</p></div>
+          <div><span><i class="fa-solid fa-phone-volume"></i></span><p>+91 293823928 </p></div>
+        </div>
+        <div className='nav-social'>
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default body_nave
---
+++ src/file/servic_data.js
@@ -0,0 +1,17 @@
+import cloudImg from "../image/cloud.png";
+import mobileImg from "../image/mobile.png";
+import bankingImg from "../image/banking.png";
+import ecommerceImg from "../image/e-commerce.avif";
+import customImg from "../image/custom-software.jpg";
+import webImg from "../image/web.jpg";
+
+let Servic_data = [
+  { id: 1, ic: "fa-solid fa-code", img: cloudImg, titl: "Cloud Solutions Development", des: "We provide comprehensive cloud solutions, from migration to management, ensuring scalability and security for your business." },
+  { id: 2, ic: "fa fa-mobile", img: mobileImg, titl: "Mobile Application Development", des: "We develop secure, high-performance mobile applications for Android and iOS. With intuitive, user-friendly designs and engaging UI/UX, our apps deliver seamless experiences across all devices." },
+  { id: 3, ic: "fa-solid fa-code", img: bankingImg, titl: "Banking Software Development", des: "We develop secure and scalable banking software solutions, including mobile banking apps and financial management systems." },
+  { id: 4, ic: "fa fa-line-chart", img: ecommerceImg, titl: "E-commerce Development", des: "We build e-commerce solutions that drive growth. Our online stores feature secure payments, streamlined product management, and a seamless shopping experience designed for high conversions." },
+  { id: 5, ic: "fa fa-cogs", img: customImg, titl: "Custom Software Development", des: "We craft tailored software solutions to solve your unique business challenges. Boost efficiency and drive innovation with applications designed to streamline your workflows." },
+  { id: 6, ic: "fa fa-link", img: webImg, titl: "Web Development", des: "We craft scalable, secure, and responsive websites using the latest technologies. Our web development services empower your business with a strong, user-centric digital presence that drives results" },
+];
+
+export default Servic_data; 
